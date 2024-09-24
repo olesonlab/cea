e0ce7851564a731b2ac9bd6657b147cbc4b3f7bb
Oleson Lab Admin <25646578+olesonlab@users.noreply.github.com>
2024-09-24 23:35:53 +0000

    Update dependencies

--- functions/install_packages_and_load_libraries.R
+++ functions/install_packages_and_load_libraries.R
@@ -8,14 +8,26 @@ install_packages_and_load_libraries <- function(cran_packages = NULL, github_pac
   # Load pacman
   library(pacman)
   
+  # Convert single strings to a vector
+  cran_packages <- if (is.character(cran_packages)) cran_packages else c(cran_packages)
+  github_packages <- if (is.character(github_packages)) github_packages else c(github_packages)
+  
   # Install and load CRAN packages if specified
   if (!is.null(cran_packages) && length(cran_packages) > 0) {
-    pacman::p_load(char = cran_packages)  # Automatically installs and loads packages
+    tryCatch({
+      pacman::p_load(char = cran_packages, character.only = TRUE)  
+    }, error = function(e) {
+      message("Error loading CRAN packages: ", e$message)
+    })
   }
   
   # Install and load GitHub packages if specified
   if (!is.null(github_packages) && length(github_packages) > 0) {
-    pacman::p_load_gh(github_packages)  # Automatically installs and loads GitHub packages
+    tryCatch({
+      pacman::p_load_gh(github_packages)  
+    }, error = function(e) {
+      message("Error loading GitHub packages: ", e$message)
+    })
   }
   
   # Ensure renv is installed and loaded
@@ -28,24 +40,21 @@ install_packages_and_load_libraries <- function(cran_packages = NULL, github_pac
   suppressMessages(invisible(capture.output(renv::snapshot())))
 }
 
-# Example usages. 
-# Add or remove packages as needed. Then source file in quarto
-# docs so that dependencies are managed within one area.
+# Example usage
 cran_packages <- c(
-  # File management
-  "fs", "here", "utils", "tools", 
-  # Data tidying
+  "fs", "here", "rio", "googlesheets4", 
   "tidyverse", "janitor", "scales", "forcats",
-  # Data visualization
-  "plotly", "DT", "gt", "patchwork", "ggokabeito", "ggtext", "ggiraph", 
+  "plotly", "DT", "gt", "patchwork", "wesanderson", "ggtext", "ggiraph", 
   "rmarkdown", "leaflet", "leaflet.esri", "shiny", "gridExtra", "ggthemes",
-  "mapview",
-  # Spatial analysis
-  "terra", "sf", "units")
+  "mapview", "gganimate", "magick", "gifski", "ggrepel",
+  "terra", "units", "rmapshaper"
+)
 
-# github_packages <- c()
+# github_packages <- c()  # Uncomment and add GitHub packages if needed
 
 install_packages_and_load_libraries(
   cran_packages = cran_packages
   # github_packages = github_packages
 )
+
+install.packages("sf", dependencies = TRUE)
